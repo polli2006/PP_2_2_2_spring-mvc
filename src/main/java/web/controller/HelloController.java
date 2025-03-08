0837b5d28b0b7471186fe9da700a61bdcc0d6a91
Polina_PC <polli2006@mail.ru>
2025-03-08 22:28:31 +0300

    2.2.2

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,9 +1,13 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.CarService;
+import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +23,16 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	private final CarService carService = new CarServiceImpl();
+
+	//http://localhost:8088/spring_mvc_war_exploded/cars?count=2
+	@GetMapping(value = "/cars")
+	public String showCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
+		List<Car> cars = carService.getCars(count);
+		model.addAttribute("cars", cars);
+		return "cars";
 	}
 	
 }
