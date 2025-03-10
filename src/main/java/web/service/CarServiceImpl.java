53d85cb2ae4187e8048a8e99d8da159c401cedf3
Polina_PC <polli2006@mail.ru>
2025-03-10 22:39:21 +0300

    2.2.2_upd

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -7,7 +7,9 @@ public class CarServiceImpl implements CarService {
     private List<Car> cars = List.of(
             new Car(123, "Toyota", "red"),
             new Car(456, "Ford", "white"),
-            new Car(789, "Lada", "green"));
+            new Car(789, "Lada", "green"),
+            new Car(401, "Mercedes", "black"),
+            new Car(712, "Niva", "yellow"));
 
     @Override
     public List<Car> getCars(Integer count) {
